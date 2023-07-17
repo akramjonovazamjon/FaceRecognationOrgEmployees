7286915ff550e60be784ce8b98694457d0cad8ea
akramjonovazamjon <mrazamjonovakramjon@gmail.com>
2023-07-17 09:19:02 +0500

    some change

--- src/main/java/com/example/lionprintfirstproject/config/SecurityConfig.java
+++ src/main/java/com/example/lionprintfirstproject/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**", "/cameras", "/images/**", "/employees/**")
+                .requestMatchers("/auth/**", "/cameras", "/images/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
