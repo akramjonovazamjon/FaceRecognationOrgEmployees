b78cae47db218ecab62909239642b6435aba02db
akramjonovazamjon <mrazamjonovakramjon@gmail.com>
2023-07-22 11:51:49 +0500

    some change

--- src/main/java/com/example/lionprintfirstproject/service/CameraService.java
+++ src/main/java/com/example/lionprintfirstproject/service/CameraService.java
@@ -1,20 +1,13 @@
 package com.example.lionprintfirstproject.service;
 
-import com.example.lionprintfirstproject.dto.CameraResponse;
 import com.example.lionprintfirstproject.dto.CameraResult;
-import com.example.lionprintfirstproject.dto.camera.*;
 import com.example.lionprintfirstproject.entity.Employee;
 import com.example.lionprintfirstproject.entity.EmployeeWorkingDay;
 import com.example.lionprintfirstproject.repository.EmployeeWorkingDayRepository;
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import java.time.Duration;
@@ -22,7 +15,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -31,7 +23,6 @@ public class CameraService {
     private final EmployeeWorkingDayRepository repository;
     private final Gson gson;
     private final EmployeeService employeeService;
-    private final RestTemplate restTemplate;
 
     public String getEmployeeId(String json) {
 
