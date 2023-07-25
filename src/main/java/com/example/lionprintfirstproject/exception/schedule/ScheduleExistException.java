17f57c2d384414f685509d31481fd62f041a3f27
asrorbek07 <asrorbekazamjon@gmail.com>
2023-07-25 15:19:20 +0500

    refactored some exception classes

--- src/main/java/com/example/lionprintfirstproject/exception/schedule/ScheduleExistException.java
+++ src/main/java/com/example/lionprintfirstproject/exception/schedule/ScheduleExistException.java
@@ -1,4 +1,7 @@
 package com.example.lionprintfirstproject.exception.schedule;
 
 public class ScheduleExistException extends RuntimeException{
+    public ScheduleExistException() {
+        super("error.schedule.already.exist.found");
+    }
 }
--- src/main/java/com/example/lionprintfirstproject/exception/schedule/ScheduleNoFoundException.java
+++ src/main/java/com/example/lionprintfirstproject/exception/schedule/ScheduleNoFoundException.java
@@ -1,4 +1,7 @@
 package com.example.lionprintfirstproject.exception.schedule;
 
 public class ScheduleNoFoundException extends RuntimeException {
+    public ScheduleNoFoundException() {
+        super("error.schedule.not.found");
+    }
 }
